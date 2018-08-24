5b3dcb1d64083156270417c4eb41e1966fe59611
Dulip Withanage <dulip.withanage@gmail.com>
2018-08-24 16:13:10 +0200

    https://gitlab.ub.uni-heidelberg.de/wit/verlag-portale/issues/208

--- controllers/catalog.py
+++ controllers/catalog.py
@@ -47,7 +47,8 @@ def series():
         
         submissions.append(submission)
 
-    submissions = sorted(submissions, cmp=seriesPositionCompare, reverse=True)
+    submissions = sorted(submissions, key=lambda s: s.attributes.get("date_submitted"), reverse=True)
+
     series.associated_items['submissions'] = submissions
 
     return locals()
