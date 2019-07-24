7a28666884c7ff6c51c11fa4991deee96c43e300
Nils Weiher <weiher@ub.uni-heidelberg.de>
2019-07-24 12:05:29 +0200

    Add file exist check to index function

--- controllers/reader.py
+++ controllers/reader.py
@@ -37,6 +37,11 @@ def index():
 
     # check if it is xml
     if str(file_id).endswith('.xml'):
+        path = os.path.join(request.folder, 'static/files/presses', myconf.take('omp.press_id'), 'monographs',
+                            submission_id, 'submission/proof', file_id)
+        if os.path.exists(path) is False:
+            raise HTTP(404)
+
 
         authors = [OMPItem(author, OMPSettings(ompdal.getAuthorSettings(author.author_id)))
                    for author in ompdal.getAuthorsBySubmission(submission_id)]
